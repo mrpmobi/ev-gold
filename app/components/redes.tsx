74db2b6ee56b6b3ac8d466e5cd77a8e7965631c1
Heitor Francisco <heitorf.costa@outlook.com>
2025-11-27 12:01:11 -0300

    feat: somente 3 niveis na rede

--- app/components/redes.tsx
+++ app/components/redes.tsx
@@ -27,9 +27,6 @@ const levelOptions = [
   { value: "1", label: "1" },
   { value: "2", label: "2" },
   { value: "3", label: "3" },
-  { value: "4", label: "4" },
-  { value: "5", label: "5" },
-  { value: "6", label: "6" },
 ];
 
 const timeOptions = [
